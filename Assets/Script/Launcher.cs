ce34f2bb34c49720eaa30334d3a8a59949647cf9
Administrator <1060409179@qq.com>
2017-03-03 22:01:41 +0800

    add

---
+++ Assets/Script/Launcher.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Launcher : MonoBehaviour {
+    public GameObject ballPrefab;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		/* 按下鼠标右键            */
+        if (Input.GetMouseButtonDown(1))
+        {
+			//实例化小球
+            Instantiate(this.ballPrefab);
+        }
+	}
+}
